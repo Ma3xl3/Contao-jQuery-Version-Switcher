8b433e47613469d414ae5a0f1cc493de32989ed1
Ma3xl3 <markus.kinzl@web-creations.at>
2013-06-11 18:38:36 +0200

    moved jquery files into module folder

--- system/modules/jQueryVersionSwitcher/config/config.php
+++ system/modules/jQueryVersionSwitcher/config/config.php
@@ -20,35 +20,35 @@ $GLOBALS['TL_JQUERY_VERSIONS'] = array(
 		'googleapis' => 'http://code.jquery.com/jquery.min.js',
 	),*/
 	'1.10.1' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.10.1.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.10.1.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.10.1.min.js',
 	),
 	'1.9.1' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.9.1.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.9.1.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.9.1.min.js',
 	),
 	'1.8.3' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.8.3.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.8.3.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.8.3.min.js',
 	),
 	'1.7.2' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.7.2.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.7.2.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.7.2.min.js',
 	),
 	'1.6.4' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.6.4.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.6.4.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.6.4.min.js',
 	),
 	'1.5.2' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.5.2.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.5.2.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.5.2.min.js',
 	),
 	'1.4.4' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.4.4.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.4.4.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.4.4.min.js',
 	),
 	'1.3.2' => array(
-		'local' => 'assets/jquery-switcher/jquery-1.3.2.min.js|static',
+		'local' => 'system/modules/jQueryVersionSwitcher/assets/js/jquery/jquery-1.3.2.min.js|static',
 		'googleapis' => 'http://code.jquery.com/jquery-1.3.2.min.js',
 	),
 );
